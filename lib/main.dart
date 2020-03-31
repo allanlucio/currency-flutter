014e7c5a8c53c15dcee28bbc9cfc66a3e2e95550
allan <luciocorreia.allan@gmail.com>
2020-03-31 14:00:14 -0300

    Currency APP done

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,16 @@ const request =
 void main() async {
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+          hintStyle: TextStyle(color: Colors.amber),
+        )),
   ));
 }
 
@@ -22,19 +32,47 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-// theme: ThemeData(
-//       hintColor: Colors.amber,
-//       primaryColor: Colors.white,
-//       inputDecorationTheme: InputDecorationTheme(
-//         enabledBorder:
-//             OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-//         focusedBorder:
-//             OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
-//         hintStyle: TextStyle(color: Colors.amber),
-//       )),
-// ));
-
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final euroController = TextEditingController();
+  final dolarController = TextEditingController();
+
+  void _realChanged(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real/_dolar).toStringAsFixed(2);
+    euroController.text = (real/_euro).toStringAsFixed(2);
+  }
+  void _dolarChanged(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar * this._dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this._dolar/_euro).toStringAsFixed(2);
+  }
+  void _euroChanged(String text){
+    if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this._euro).toStringAsFixed(2);
+    dolarController.text = (euro * this._euro/_dolar).toStringAsFixed(2);
+  }
+
+  void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+  }
+
+  double _dolar;
+  double _euro;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,13 +107,49 @@ class _HomeState extends State<Home> {
                     style: TextStyle(color: Colors.amber, fontSize: 25.0),
                   ),
                 );
-              }else{
-                return Container(color:Colors.green);
+              } else {
+                _dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                _euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(
+                        Icons.monetization_on,
+                        size: 150,
+                        color: Colors.amber,
+                      ),
+                      
+                      buildTextField("Reais","R\$",realController,_realChanged),
+                      Divider(),
+                      buildTextField("Dólares","US\$",dolarController,_dolarChanged),
+                      Divider(),
+                      buildTextField("Euros","EUR",euroController, _euroChanged),
+                      
+                    ],
+                  ),
+                );
               }
-
           }
         },
       ),
     );
   }
+}
+
+Widget buildTextField(String label, String preffix, TextEditingController controller, Function onChanged) {
+  return TextField(
+    controller: controller,
+    onChanged: onChanged,
+    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(
+          color: Colors.amber,
+        ),
+        border: OutlineInputBorder(),
+        prefixText: preffix),
+  );
 }
